3730e5ecc3e949d67023a4435ce91debabd8191d
OpenByte <development.openbyte@gmail.com>
2022-07-09 14:10:34 +0200

    Refactor benches

---
+++ benches/bench.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate test;
+
+use test::Bencher;
+
+#[bench]
+fn bench_get_last_error(b: &mut Bencher) {
+    b.iter(|| get_last_error::Win32Error::new(0).to_string());
+}
+
+#[bench]
+fn bench_w32_error(b: &mut Bencher) {
+    b.iter(|| w32_error::W32Error::new(0).to_string());
+}
+
+#[bench]
+fn bench_rust_win32error(b: &mut Bencher) {
+    b.iter(|| rust_win32error::Win32Error::from(0).to_string());
+}
